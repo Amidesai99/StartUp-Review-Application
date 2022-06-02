73e9c50e09badf23d7ed4df3e8b3c2fb71c8cd04
Amidesai99 <72565845+Amidesai99@users.noreply.github.com>
2022-06-02 19:20:02 -0400

    Created and configured Database/connected Frontend and Backend

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,47 @@
-
+import React, {useState,useEffect} from "react";
 import './App.css';
+import Axios from 'axios'
+
 
 function App() {
+
+  const [startUpName,setStartUpName]= useState('');
+  const [startUpReview,setStartUpReview]= useState('');
+  const [startUpReviewList,setStartUpList] = useState([])
+
+  useEffect(() => {
+    Axios.get("http://localhost:3001/api/get").then((response)=>{
+      setStartUpList(response.data);
+    })
+  }, [])
+
+  const submitReview =()=>{
+    Axios.post("http://localhost:3001/api/insert",{
+      startUpName:startUpName,
+      startUpReview:startUpReview,
+    }).then(()=>{
+      alert("successful insert");
+    });
+  };
   return (
     <div className="App">
       <h1>STARTUP REVIEW APPLICATION</h1>
 
       <div className="form">
       <label > STARTUP NAME: </label>
-      <input type="text" name="startUPName" />
+      <input type="text" name="startUPName" onChange={(e)=>{
+        setStartUpName(e.target.value);
+      }}
+
+       />
       <label > REVIEW: </label>
-      <input type="text" name="review" />
-      <button>Submit</button>
+      <input type="text" name="review" onChange={(e)=>{
+        setStartUpReview(e.target.value);
+      }} />
+      <button onClick={submitReview}>Submit</button>
+      {startUpReviewList.map((val)=>{
+        return <h1>StartUpName:{val.startUpName}  | StartUp Review : {val.startUpReview}</h1>
+      })}
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,12 +1,41 @@
-const express = require('express')
-const app = express()
-
-app.get("/", (req,res)=>{
-    res.send("hello world");
-})
-
+const express = require("express");
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const mysql = require("mysql2");
 
 
-app.listen(3001, () =>{
+const db = mysql.createPool({
+    host:"localhost",
+    user:"root",
+    password:"Divahottie@99",
+    database:"CRUDDataBase",
+});
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get('/api/get', (req,res)=>{
+    const sqlSelect = "SELECT * FROM startup_reviews";
+    db.query(sqlSelect,  (err,result)=> {
+        res.send(result);
+    });    
+
+});
+
+
+app.post("/api/insert", (req,res)=>{
+
+    const startUpName = req.body.startUpName;
+    const startUpReview = req.body.startUpReview;
+    const sqlInsert = "INSERT INTO startup_reviews(startUpName,startUpReview) VALUES (?,?)";
+    db.query(sqlInsert, [startUpName,startUpReview], (err,result)=> {
+        console.log(result);
+
+
+    });
+});
+
+app.listen(3001, () => {
     console.log("running on port 3001");
 });
